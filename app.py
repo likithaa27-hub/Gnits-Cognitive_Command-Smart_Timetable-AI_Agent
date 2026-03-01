b3ac9c5b49f72cacf43b39bc10e1b4b1823fe1e9
Likitha A <likitha.a27@gmail.com>
2026-03-01 16:20:13 +0530

    Week 1 basic setup completed

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("AI Smart Timetable Agent")
+
+st.write("Week 1: Project Setup Completed")
+
+event = st.text_input("Enter event name")
+
+if st.button("Add Event"):
+    st.success(f"Event '{event}' added successfully!")
